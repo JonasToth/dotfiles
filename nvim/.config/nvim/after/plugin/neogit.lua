607c1032b813f467cf8a5f68d495ae15fac77d74
Jonas Toth <development@jonas-toth.eu>
2024-07-25 23:06:32 +0200

    [nvim] fix neogit commit usage

--- nvim/.config/nvim/after/plugin/neogit.lua
+++ nvim/.config/nvim/after/plugin/neogit.lua
@@ -1,9 +1,12 @@
 local neogit = require('neogit')
 neogit.setup({
     graph_style = "ascii",
-
+    commit_editor = {
+        kind = "replace",
+    }
 })
-vim.keymap.set("n", "<leader>gs", function () neogit.open({ kind = "floating" }) end)
+-- kind = "floating" makes a floating pane, but fails to properly commit.
+vim.keymap.set("n", "<leader>gs", function() neogit.open({ kind = "tab" }) end)
 vim.keymap.set("n", "<leader>gw", ":Gwrite<CR>")
 vim.keymap.set("n", "<leader>gc", ":Neogit commit<CR>")
 vim.keymap.set("n", "<leader>gp", ":Neogit push<CR>")
