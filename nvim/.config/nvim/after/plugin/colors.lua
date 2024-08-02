8eca1828716bfe538dbf30f1496c1574bd93f20b
Jonas Toth <jto@ivu.de>
2024-08-02 11:03:35 +0200

    [nvim] change color constrast

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,6 +1,6 @@
 -- Install the color scheme right after plugin section.
 vim.opt.background = "dark"
-vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_background = "soft"
 vim.g.gruvbox_material_better_performance = 1
 vim.g.gruvbox_material_disable_italic_comment = true
 vim.cmd("silent! colorscheme gruvbox-material")
