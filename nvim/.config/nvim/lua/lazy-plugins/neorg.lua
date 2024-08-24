d4afed89af994f090eeaf1d39a6ae0d27971ec41
Jonas Toth <development@jonas-toth.eu>
2024-08-24 21:56:44 +0200

    [nvim] improve neorg setup

--- nvim/.config/nvim/lua/lazy-plugins/neorg.lua
+++ nvim/.config/nvim/lua/lazy-plugins/neorg.lua
@@ -11,7 +11,15 @@ return {
     },
     cmd = "Neorg",
     keys = {
-        { "<leader>N", ":Neorg<CR>", desc = "Open Neorg Menu" },
+        { "<leader>N",   ":Neorg<CR>",                    desc = "Open Neorg Menu" },
+        { "<leader>ni",  ":Neorg index<CR>",              desc = "Open Index" },
+        { "<leader>nl",  ":Neorg toc<CR>",                desc = "Open Table of Contents" },
+        { "<leader>njl", ":Neorg journal toc<CR>",        desc = "Open Journal Table Of Contents" },
+        { "<leader>njt", ":Neorg journal today<CR>",      desc = "Open Today Journal" },
+        { "<leader>njc", ":Neorg journal custom<CR>",     desc = "Open Custom Journal" },
+        { "<leader>njf", ":Neorg journal tomorrow<CR>",   desc = "Open Tomorrow's Journal" },
+        { "<leader>njy", ":Neorg journal yesterday<CR>",  desc = "Open Yesterday's Journal" },
+        { "<leader>nn",  "<Plug>(neorg.dirman.new-note)", desc = "Create a New Note" },
     },
     config = function()
         require("neorg").setup({
@@ -34,8 +42,22 @@ return {
                     },
                 },
                 ["core.todo-introspector"] = {},
-                ["core.esupports.metagen"] = {},
+                ["core.esupports.metagen"] = {
+                    config = {
+                        author = "Jonas Toth",
+                        type = "auto",
+                    }
+                },
+                ["core.ui.calendar"] = {},
+                ["core.journal"] = {},
+                ["core.keybinds"] = {
+                    config = {
+                        default_keybinds = true,
+                    }
+                }
             },
         })
+        vim.wo.foldlevel = 99
+        vim.wo.conceallevel = 2
     end,
 }
