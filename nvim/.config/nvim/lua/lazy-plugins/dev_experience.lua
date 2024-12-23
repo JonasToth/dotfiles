886fa36a5fb8d505af6835bdb59665c7328ab2b3
Jonas Toth <development@jonas-toth.eu>
2024-12-23 15:17:52 +0100

    nvim: better vim.ui.input configuration from snacks

--- nvim/.config/nvim/lua/lazy-plugins/dev_experience.lua
+++ nvim/.config/nvim/lua/lazy-plugins/dev_experience.lua
@@ -22,11 +22,44 @@ return {
         event = "VeryLazy",
     },
     {
-        "echasnovski/mini.nvim",
+        "folke/snacks.nvim",
         event = "VeryLazy",
-        config = function()
-            -- require("mini.surround").setup({})
-            require('mini.pairs').setup({})
-        end,
-    },
+        ---@type snacks.Config
+        opts = {
+            input = {
+                backdrop = false,
+                position = "float",
+                border = "rounded",
+                title_pos = "center",
+                height = 1,
+                width = 60,
+                relative = "editor",
+                noautocmd = true,
+                row = 2,
+                -- relative = "cursor",
+                -- row = -3,
+                -- col = 0,
+                wo = {
+                    winhighlight =
+                    "NormalFloat:SnacksInputNormal,FloatBorder:SnacksInputBorder,FloatTitle:SnacksInputTitle",
+                    cursorline = false,
+                },
+                bo = {
+                    filetype = "snacks_input",
+                    buftype = "prompt",
+                },
+                --- buffer local variables
+                b = {
+                    completion = false, -- disable blink completions in input
+                },
+                keys = {
+                    n_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n" },
+                    i_esc = { "<esc>", { "cmp_close", "stopinsert" }, mode = "i" },
+                    i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = "i" },
+                    i_tab = { "<tab>", { "cmp_select_next", "cmp" }, mode = "i" },
+                    q = "cancel",
+                },
+            }
+        }
+    }
 }
