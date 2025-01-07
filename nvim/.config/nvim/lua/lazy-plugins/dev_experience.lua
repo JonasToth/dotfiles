b74c64e2c7b6ec6f9f3c41e9a725d80ea380b6e1
Jonas Toth <development@jonas-toth.eu>
2025-01-07 21:40:56 +0100

    nvim: improve snacks.nvim

--- nvim/.config/nvim/lua/lazy-plugins/dev_experience.lua
+++ nvim/.config/nvim/lua/lazy-plugins/dev_experience.lua
@@ -29,11 +29,7 @@ return {
 		"kylechui/nvim-surround",
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
 		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
+        opts = {},
 	},
 	{
 		-- Improved UI-input widget.
@@ -41,7 +37,10 @@ return {
 		event = "VeryLazy",
 		---@type snacks.Config
 		opts = {
+            notifier = { enabled = true },
+            quickfile = { enabled = true },
 			input = {
+                enabled = true,
 				backdrop = false,
 				position = "float",
 				border = "rounded",
