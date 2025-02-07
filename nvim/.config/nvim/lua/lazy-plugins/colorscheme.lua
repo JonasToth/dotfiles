e3d00782bce17ef996ed5b4ee53a592baaae9954
Jonas Toth <development@jonas-toth.eu>
2025-02-07 21:13:52 +0100

    nvim: update colorscheme to nicer dark one

--- nvim/.config/nvim/lua/lazy-plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/lazy-plugins/colorscheme.lua
@@ -1,15 +1,56 @@
 return {
+    -- {
+    --     "sainnhe/sonokai",
+    --     priority = 1000,
+    --     lazy = false,
+    --     config = function()
+    --         vim.g.sonokai_enable_italic = true
+    --         vim.g.sonokai_better_performance = false
+    --         -- require('sonokai').setup({})
+    --         vim.cmd("colorscheme sonokai")
+    --     end,
+    -- },
     {
-        "sainnhe/sonokai",
-        priority = 1000,
+        "dgox16/oldworld.nvim",
         lazy = false,
+        priority = 1000,
         config = function()
-            vim.g.sonokai_enable_italic = true
-            vim.g.sonokai_better_performance = false
-            -- require('sonokai').setup({})
-            vim.cmd("colorscheme sonokai")
+            require("oldworld").setup({
+                integrations = { -- You can disable/enable integrations
+                    alpha = true,
+                    cmp = true,
+                    flash = true,
+                    gitsigns = true,
+                    hop = true,
+                    indent_blankline = true,
+                    lazy = true,
+                    lsp = true,
+                    markdown = true,
+                    mason = true,
+                    navic = false,
+                    neo_tree = true,
+                    neogit = true,
+                    neorg = true,
+                    noice = false,
+                    notify = true,
+                    rainbow_delimiters = true,
+                    telescope = true,
+                    treesitter = true,
+                },
+            })
+            vim.cmd("colorscheme oldworld")
         end,
     },
+    -- {
+    --     "vague2k/vague.nvim",
+    --     config = function()
+    --         -- NOTE: you do not need to call setup if you don't want to.
+    --         require("vague").setup({
+    --             -- optional configuration here
+    --         })
+    --         vim.cmd("colorscheme vague")
+    --     end
+    -- },
     {
         -- Dimming inactive parts of the code to get higher focus.
         "folke/twilight.nvim",
@@ -71,5 +112,7 @@ return {
                 modemsg = false,
             },
         },
+    },
+    {
     },
 }
