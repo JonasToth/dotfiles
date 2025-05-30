6a992150fae372e1f5480fce6b196e7669dcd69f
Jonas Toth <development@jonas-toth.eu>
2025-05-30 23:34:40 +0200

    hypr: wlogout row style

--- waybar/.config/waybar/scripts/power.sh
+++ waybar/.config/waybar/scripts/power.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 wlogout --protocol layer-shell \
-        --buttons-per-row 2 \
-        --column-spacing 50 \
+        --buttons-per-row 6 \
+        --column-spacing 0 \
         --row-spacing 50 \
         --margin-left 400 \
         --margin-right 400
