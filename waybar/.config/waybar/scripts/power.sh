a8f2c87e1f4b46b79778be88cac2ea207103294e
Jonas Toth <development@jonas-toth.eu>
2025-06-01 20:55:59 +0200

    hypr: adjust wlogout appearance and change font family to ubuntu

--- waybar/.config/waybar/scripts/power.sh
+++ waybar/.config/waybar/scripts/power.sh
@@ -4,5 +4,7 @@ wlogout --protocol layer-shell \
         --buttons-per-row 6 \
         --column-spacing 0 \
         --row-spacing 50 \
-        --margin-left 400 \
-        --margin-right 400
+        --margin-left 600 \
+        --margin-right 600 \
+        --margin-top 400 \
+        --margin-bottom 400
